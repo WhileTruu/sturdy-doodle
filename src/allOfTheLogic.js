e5b4909b2adad5601fc6adcf6e22d787f68a23ff
Marten Truu <marten.truu@gmail.com>
2016-10-17 20:32:41 +0300

    tomato

--- src/allOfTheLogic.js
+++ src/allOfTheLogic.js
@@ -131,7 +131,7 @@ export function handlePostRequest(request, response) {
   if (path !== '/file' || !('id' in params)) {
     response.writeHead(404, { 'Content-Type': 'text/plain' })
     response.end()
-  } else if (`${downloadIp}` == 'localhost' || `${downloadIp}:${port}` == `::ffff:127.0.0.1:${PROCESS_ARGUMENTS.port}`) {
+  } else if (`${downloadIp}` == '127.0.0.1' || `${downloadIp}:${port}` == `::ffff:127.0.0.1:${PROCESS_ARGUMENTS.port}`) {
     let content = ''
     request.on('data', (chunk) => content += chunk)
     request.on('end', () => {
